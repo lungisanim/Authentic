b05ea367fb0acc223ae5cb350932f592a06a0434
lungisani <lm.nhlengethwa@outlook.com>
2017-03-07 15:27:29 +0200

    Professional branch committed

--- Test/Test.java
+++ Test/Test.java
@@ -1,44 +1,24 @@
+import java.math.BigInteger;
 import java.util.Scanner;
-
-/**
- * Created by lungisani on 2017/02/27.
- */
 public class Test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String identify = String.valueOf(in.next());
-        int length = identify.length() - 1;
-        String identity = identify.substring(0, length);
-        String check = identify.substring(12, 13);
-        int checksum = Integer.valueOf(check);
-        char[] chars = identity.toCharArray();
-        int size = chars.length;
-        int sumOdd = 0, sumEven, sumDoubled = 0;
-        int sum = 0, _doubled;
-        for (int x = 0; x < length; x++) {
-            if (x % 2 == 0) {
-                String numString = String.valueOf(chars[x]);
-                int numbers = Integer.valueOf(numString);
-                sumOdd += numbers;
-            } else if (x % 2 != 0) {
-                String numString = String.valueOf(chars[x]);
-                int numbers = Integer.valueOf(numString);
-                int doubled = numbers * 2;
-                if (doubled > 9) {
-                    _doubled = doubled - 9;
-                }else {
-                    _doubled = doubled;
-                }
-                sumDoubled += _doubled;
+        BigInteger identities = in.nextBigInteger();
+        char[] identify = identities.toString().toCharArray();
+        int summation = 0, sumEven = 0, sumOdd = 0;
+        for (int x = 0; x < identify.length - 1; x++) {
+            int numbers = Character.getNumericValue(identify[x]);
+            if ((x % 2) != 0) {
+                sumOdd += numbers < 5 ? numbers * 2 : numbers * 2 - 9;
+            } else {
+                sumEven += numbers;
             }
+
         }
-        sumEven =+ sumDoubled;
-        int summation = sumOdd + sumEven;
-        int sums = summation * 9;
-        if (sums % 10 == checksum) {
-            System.out.println("Valid Identity number.");
-        } else {
-            System.out.println("Invalid Identity number.");
-        }
+        summation = sumEven + sumOdd;
+        int checksum = Character.getNumericValue(identify[identify.length - 1]);
+        int algorithm = (summation * 9) % 10;
+
+        System.out.println(summation);
     }
 }
